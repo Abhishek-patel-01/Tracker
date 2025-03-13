ca58f05cca771ac99105ba642deb6b596518cf0e
Abhishek Patel <166133349+Abhishek-patel-01@users.noreply.github.com>
2025-03-13 15:34:08 +0530

    Initial commit

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,45 +1,67 @@
-const socket = io();
+document.addEventListener("DOMContentLoaded", () => {
+  const userIdentifier = window.location.pathname.split("/").pop();
+  const socket = io({ query: { userIdentifier } });
 
-if (navigator.geolocation) {
-  navigator.geolocation.watchPosition(
-    (position) => {
-      const { latitude, longitude } = position.coords;
-      socket.emit("send-location", { latitude, longitude });
-    },
-    (error) => {
-      console.log(error);
-    },
-    {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0,
+  const map = L.map("map").setView([0, 0], 2);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Map data &copy; OpenStreetMap",
+  }).addTo(map);
+
+  const markers = {};
+  let routePolyline = null;
+
+  function fetchLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(
+        (position) => {
+          socket.emit("send-location", {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error("Geolocation error:", error);
+          alert("Could not get your location.");
+        },
+        { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000 }
+      );
+    } else {
+      alert("Geolocation is not supported.");
     }
-  );
-}
-
-const map = L.map("map").setView([0, 0], 10);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "Abhishek Patel",
-}).addTo(map);
-
-const markers = {};
-
-socket.on("receive-location", (data) => {
-  const { id, latitude, longitude } = data;
-  map.setView([latitude, longitude], 16);
-
-  if (markers[id]) {
-    markers[id].setLatLng([latitude, longitude]);
-  } else {
   }
 
-  markers[id] = L.marker([latitude, longitude]).addTo(map);
-});
+  fetchLocation();
 
-socket.on("user-disconnected", (id) => {
-  if (markers[id]) {
-    map.removeLayer(markers[id]);
-    delete markers[id];
-  }
+  socket.on("receive-location", (data) => {
+    if (markers[data.userIdentifier]) {
+      markers[data.userIdentifier].setLatLng([data.latitude, data.longitude]);
+    } else {
+      markers[data.userIdentifier] = L.marker([
+        data.latitude,
+        data.longitude,
+      ]).addTo(map);
+    }
+    map.setView([data.latitude, data.longitude], 16);
+  });
+
+  document.getElementById("calculate-route").addEventListener("click", () => {
+    const user1 = prompt("Enter first user identifier:");
+    const user2 = prompt("Enter second user identifier:");
+    if (user1 && user2) {
+      socket.emit("calculate-route", {
+        user1Identifier: user1,
+        user2Identifier: user2,
+      });
+    }
+  });
+
+  socket.on("route-drawn", (data) => {
+    if (routePolyline) map.removeLayer(routePolyline);
+    routePolyline = L.polyline(data.routeCoordinates, {
+      color: "blue",
+    }).addTo(map);
+    map.fitBounds(routePolyline.getBounds());
+  });
+
+  socket.on("route-error", (msg) => alert(msg));
 });
--- app.js
+++ app.js
@@ -2,40 +2,78 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const path = require("path");
-
 const socketio = require("socket.io");
+const axios = require("axios");
 
 const server = http.createServer(app);
-
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.use(express.static(path.join("public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(
-  "/css",
-  express.static("public/css", {
-    setHeaders: (res, path) => {
-      if (path.endsWith(".css")) {
-        res.setHeader("Content-Type", "text/css");
-      }
-    },
-  })
-);
+const trackedUsers = {}; // Store location by unique identifier
+
+app.get("/track/:identifier", function (req, res) {
+  res.render("index", { identifier: req.params.identifier });
+});
 
 io.on("connection", function (socket) {
+  const userIdentifier = socket.handshake.query.userIdentifier;
+  socket.userIdentifier = userIdentifier;
+  console.log("A user connected:", userIdentifier);
+
   socket.on("send-location", function (data) {
-    io.emit("receive-location", { id: socket.id, ...data });
+    if (userIdentifier) {
+      trackedUsers[userIdentifier] = data;
+      io.sockets.emit("receive-location", {
+        id: socket.id,
+        userIdentifier,
+        ...data,
+      });
+    }
   });
-  console.log("connected");
+
+  socket.on("calculate-route", async (data) => {
+    const { user1Identifier, user2Identifier } = data;
+    const start = trackedUsers[user1Identifier];
+    const end = trackedUsers[user2Identifier];
+
+    if (start && end) {
+      try {
+        const orsApiKey =
+          "5b3ce3597851110001cf62483e92f555d7f946399d5fa43372c5ad47"; // Replace with your actual API key
+        const response = await axios.get(
+          `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${orsApiKey}&start=${start.longitude},${start.latitude}&end=${end.longitude},${end.latitude}`
+        );
+
+        const routeCoordinates =
+          response.data.features[0].geometry.coordinates.map((coord) => [
+            coord[1],
+            coord[0],
+          ]);
+
+        io.sockets.emit("route-drawn", {
+          user1Identifier,
+          user2Identifier,
+          routeCoordinates,
+        });
+      } catch (error) {
+        console.error("Route calculation error:", error);
+        socket.emit("route-error", "Error calculating route.");
+      }
+    } else {
+      socket.emit(
+        "route-error",
+        "Could not find locations for selected users."
+      );
+    }
+  });
 
   socket.on("disconnect", function () {
-    io.emit("user-disconnected", socket.id);
+    delete trackedUsers[userIdentifier];
   });
 });
 
-app.get("/", function (req, res) {
-  res.render("index");
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
-
-server.listen(3000);
